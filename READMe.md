6dbb2f1990b17c070567ad300b5f9cf54f12591e
Sriram <ramsriramram9@gmail.com>
2026-07-14 12:17:55 +0530

    Creating repo

---
+++ READMe.md
@@ -0,0 +1,2 @@
+Hello Users, This is Vijay
+Trying to figure out some use cases that i encountered in day to day development
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request
+import puremagic
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message":"File Validation API"})
+
+@app.route("/validate", methods=["POST"])
+def validate():
+
+    if "file" not in request.files:
+        return jsonify({
+            "success": False,
+            "message": "No file uploaded"
+        }), 400
+
+    file = request.files["file"]
+
+    file.stream.seek(0)
+    from_stream = puremagic.from_stream(file.stream)
+
+    file.stream.seek(0)
+    magic_stream = puremagic.magic_stream(file.stream)
+
+    return jsonify({
+        "success": True,
+        "filename": file.filename,
+        "from_stream": from_stream,
+        "matches": [
+            {
+                "extension": m.extension,
+                "confidence": m.confidence
+            }
+            for m in magic_stream
+        ]
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
